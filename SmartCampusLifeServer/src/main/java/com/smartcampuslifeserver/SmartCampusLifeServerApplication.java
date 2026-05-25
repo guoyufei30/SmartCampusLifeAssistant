6a87a37195be0cd304ab197b211161b35093036d
sightf <3583932483@qq.com>
2026-05-25 22:44:46 +0800

    server端

---
+++ SmartCampusLifeServer/src/main/java/com/smartcampuslifeserver/SmartCampusLifeServerApplication.java
@@ -0,0 +1,13 @@
+package com.smartcampuslifeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusLifeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusLifeServerApplication.class, args);
+    }
+
+}
---
+++ SmartCampusLifeServer/src/test/java/com/smartcampuslifeserver/SmartCampusLifeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampuslifeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusLifeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
